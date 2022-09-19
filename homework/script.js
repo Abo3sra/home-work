73199ee5ebd3243e8cee9653943fe0fdf3c24733
Ahmed Assiri <113802021+Abo3sra@users.noreply.github.com>
2022-09-19 14:21:51 +0300

    Add files via upload

---
+++ homework/script.js
@@ -0,0 +1,21 @@
+
+
+let container = document.getElementById("root")
+
+const element = (
+  <div>
+    <h1>Ahmed Assiri CV</h1>
+
+    <p>A passionate Computer Programmer seeking for an opportunity to apply my educational background, enhance my skills and strengthen my knowledge. I am willing to explore a wide variety of opportunities that can help me gain exposure and experience in several sectors.</p>
+    <div id="i" >
+
+        
+    <img src="https://archello.s3.eu-central-1.amazonaws.com/images/2021/05/26/pedrali-natu-restaurant-restaurants-archello.1622040571.4411.jpg"></img>
+    </div>
+    <h2>2019 – 2021
+High diploma of Computer programming / institute of public administration (first of Class), Riyadh
+</h2>
+  </div>
+);
+ReactDOM.render(element, container);
+
